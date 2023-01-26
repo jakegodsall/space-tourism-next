99d84d52cbb9911f13b6b311a772160619cf2655
jakegodsall <jake.edward.godsall@gmail.com>
2023-01-26 07:00:42 +0000

    added background images to tailwind config

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,9 +3,9 @@ import '@/styles/globals.scss';
 
 export default function App({ Component, pageProps }) {
     return (
-        <>
+        <div className='w-screen h-screen '>
             <Header />
             <Component {...pageProps} />
-        </>
+        </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,28 @@ module.exports = {
             barlow: ['Barlow Condensed', 'sans-serif'],
             bell: ['Bellefair', 'serif'],
         },
-        extend: {},
+        extend: {
+            backgroundImage: {
+                'home-desktop': 'url("../public/images/home/background-home-desktop.jpg")',
+                'home-tablet': 'url("../public/images/home/background-home-tablet.jpg")',
+                'home-mobile': 'url("../public/images/home/background-home-mobile.jpg")',
+                'destination-desktop':
+                    'url("../public/images/destination/background-destination-desktop.jpg")',
+                'destination-tablet':
+                    'url("../public/images/destination/background-destination-tablet.jpg")',
+                'destination-mobile':
+                    'url("../public/images/destination/background-destination-mobile.jpg")',
+                'crew-desktop': 'url("../public/images/crew/background-crew-desktop.jpg")',
+                'crew-tablet': 'url("../public/images/crew/background-crew-tablet.jpg")',
+                'crew-mobile': 'url("../public/images/crew/background-crew-mobile.jpg")',
+                'technology-desktop':
+                    'url("../public/images/technology/background-technology-desktop.jpg")',
+                'technology-tablet':
+                    'url("../public/images/technology/background-technology-tablet.jpg")',
+                'technology-mobile':
+                    'url("../public/images/technology/background-technology-mobile.jpg")',
+            },
+        },
     },
     plugins: [],
 };

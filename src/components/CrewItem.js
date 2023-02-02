8b1f1d0f8ddf6312c26c64093e71424e45d74976
jakegodsall <jake.edward.godsall@gmail.com>
2023-02-02 06:55:54 +0000

    added desktop layout and styling

--- src/components/CrewItem.js
+++ src/components/CrewItem.js
@@ -28,11 +28,11 @@ const CrewItem = (props) => {
             <p className='text-white text-xl uppercase font-barlow tracking-[4px] my-6 md:text-2xl md:self-start md:mb-8 duration-1000'>
                 <span className='text-[#888]'>02</span> Meet your crew
             </p>
-            <div className='grid grid-rows-[320px, auto] items-center mt-5'>
-                <div className='h-[320px] border-b-2 border-[#555] md:row-start-2 relative md:h-[400px] md:border-b-0'>
+            <div className='grid grid-rows-[320px, auto] items-center mt-5 lg:grid-rows-1 lg:grid-cols-2'>
+                <div className='h-[320px] border-b-2 border-[#555] md:row-start-2 relative md:h-[400px] md:border-b-0 lg:row-start-1 lg:col-start-2 lg:h-[800px]'>
                     <Image src={imageSrc} alt={name} className='m-auto object-contain' fill></Image>
                 </div>
-                <div className='h-full w-full px-5 mx-auto flex flex-col items-center text-center md:row-start-1 md:relative md:h-[300px] md:w-[75%]'>
+                <div className='h-full w-full px-5 mx-auto flex flex-col items-center text-center md:row-start-1 md:relative md:h-[300px] md:w-[75%] lg:h-full lg:justify-center'>
                     <div className='my-6 md:hidden'>{props.children}</div>
                     <p className='text-[#888] font-bell uppercase text-2xl'>{role}</p>
                     <p className='text-[#fff] font-bell uppercase text-4xl md:text-5xl my-4'>
@@ -41,7 +41,7 @@ const CrewItem = (props) => {
                     <p className='text-[#d0d6f9] font-barlow text-lg font-light tracking-wider leading-7 text-center'>
                         {bio}
                     </p>
-                    <div className='hidden md:block md:absolute md:bottom-[2rem]'>
+                    <div className='hidden md:block md:absolute md:bottom-[5rem] md:left-[2rem]'>
                         {props.children}
                     </div>
                 </div>

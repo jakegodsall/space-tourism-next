db72c338edbec5480551bf1a3c7760be3a505e54
jakegodsall <jake.edward.godsall@gmail.com>
2023-02-04 05:47:33 +0000

    added alt to logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Image from 'next/image';
 
 import NavBar from './NavBar';
@@ -5,7 +6,9 @@ import NavBar from './NavBar';
 const Header = () => {
     return (
         <header className='flex justify-between items-center w-full pl-5 pt-7'>
-            <Image src='/images/shared/logo.svg' width={50} height={50} />
+            <Link href='/'>
+                <Image src='/images/shared/logo.svg' width={50} height={50} alt='logo' />
+            </Link>
             <NavBar />
         </header>
     );

ca7a2f0cd851985baef172fd90c055929e6c0b76
jakegodsall <jake.edward.godsall@gmail.com>
2023-02-07 18:34:12 +0000

    fixed styling and layout for desktop

--- src/components/CrewItem.js
+++ src/components/CrewItem.js
@@ -23,25 +23,26 @@ const CrewItem = (props) => {
 
     return (
         <div className='flex flex-col items-center m-8 duration-1000'>
-            <p className='text-white text-xl uppercase font-barlow tracking-[4px] my-6 md:text-2xl md:self-start md:mb-8 duration-1000'>
+            <p className='text-white text-xl uppercase font-barlow tracking-[4px] my-6 md:text-2xl md:self-start md:mb-8 lg:hidden duration-1000'>
                 <span className='text-[#888]'>02</span> Meet your crew
             </p>
             <div className='grid grid-rows-[320px, auto] items-center mt-5 lg:grid-rows-1 lg:grid-cols-2'>
                 <div className='h-[320px] border-b-2 border-[#555] md:row-start-2 relative md:h-[400px] md:border-b-0 lg:row-start-1 lg:col-start-2 lg:h-[60vh]'>
                     <Image src={imageSrc} alt={name} className='m-auto object-contain' fill></Image>
                 </div>
-                <div className='h-full w-full px-5 mx-auto flex flex-col items-center text-center md:row-start-1 md:relative md:h-[300px] md:w-[75%] lg:h-full lg:justify-center'>
+                <div className='h-full w-full px-5 mx-auto flex flex-col items-center text-center md:row-start-1 md:relative md:h-[300px] md:w-[75%] lg:h-full lg:justify-center lg:text-left lg:relative'>
+                    <p className='text-white text-xl uppercase font-barlow tracking-[4px] my-6 md:text-xl md:self-start md:mb-8 hidden lg:block duration-1000 absolute top-0'>
+                        <span className='text-[#888]'>02</span> Meet your crew
+                    </p>
                     <div className='my-6 md:hidden'>{props.children}</div>
-                    <p className='text-[#888] font-bell uppercase text-2xl'>{role}</p>
-                    <p className='text-[#fff] font-bell uppercase text-4xl md:text-5xl my-4'>
+                    <p className='text-[#888] font-bell uppercase text-2xl lg:self-start'>{role}</p>
+                    <p className='text-[#fff] font-bell uppercase text-4xl md:text-5xl my-4 lg:self-start'>
                         {name}
                     </p>
-                    <p className='text-[#d0d6f9] font-barlow text-lg font-light tracking-wider leading-7 text-center'>
+                    <p className='text-[#2a2b35] font-barlow text-lg font-light tracking-wider leading-7 text-center lg:text-left'>
                         {bio}
                     </p>
-                    <div className='hidden md:block md:absolute md:bottom-[5rem] md:left-[2rem]'>
-                        {props.children}
-                    </div>
+                    <div className='hidden md:block md:absolute bottom-10'>{props.children}</div>
                 </div>
             </div>
         </div>

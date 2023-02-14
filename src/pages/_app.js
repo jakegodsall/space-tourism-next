cf5fa27260ae190fcf4701c0b49a1efc44695212
jakegodsall <jake.edward.godsall@gmail.com>
2023-02-14 06:26:58 +0000

    added page transitions with Framer Motion

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,13 @@
 import Header from '@/components/Header';
 import '@/styles/globals.scss';
+import '../styles/transition.css';
+
+import Transition from '@/components/Transition';
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <Transition>
+            <Component {...pageProps} />;
+        </Transition>
+    );
 }
